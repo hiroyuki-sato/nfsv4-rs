b8ec9afb52887e7f34270f315fab11d887f7d08b
Hiroyuki Sato <hiroysato@gmail.com>
2026-03-20 01:21:12 +0900

    Add Time4 encode/decode implementation

--- src/nfsv4/types/time4.rs
+++ src/nfsv4/types/time4.rs
@@ -1,10 +1,79 @@
 #![allow(dead_code)]
 
-use super::*;
+use crate::nfsv4::types::Nfsv4Error;
+
+use xdr_rs::reader::XdrReader;
+use xdr_rs::writer::XdrWriter;
 
 /// RFC7531: nfstime4
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct NfsTime4 {
     pub seconds: i64,
     pub nseconds: u32,
+}
+
+impl NfsTime4 {
+    pub fn decode(r: &mut XdrReader) -> Result<Self, Nfsv4Error> {
+        let seconds = r.read_i64()?;
+        let nseconds = r.read_u32()?;
+        Ok(Self { seconds, nseconds })
+    }
+    pub fn encode(&self, w: &mut XdrWriter) -> Result<(), Nfsv4Error> {
+        w.write_i64(self.seconds)?;
+        w.write_u32(self.nseconds)?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_nfstime4_encode_decode() {
+        let time = NfsTime4 {
+            seconds: 123456789,
+            nseconds: 987654321,
+        };
+
+        let mut w = XdrWriter::new();
+        time.encode(&mut w).unwrap();
+
+        let mut r = XdrReader::new(w.as_bytes());
+        let decoded = NfsTime4::decode(&mut r).unwrap();
+
+        assert_eq!(time, decoded);
+    }
+
+    #[test]
+    fn test_nfstime4_zero() {
+        let time = NfsTime4 {
+            seconds: 0,
+            nseconds: 0,
+        };
+
+        let mut w = XdrWriter::new();
+        time.encode(&mut w).unwrap();
+
+        let mut r = XdrReader::new(w.as_bytes());
+        let decoded = NfsTime4::decode(&mut r).unwrap();
+
+        assert_eq!(time, decoded);
+    }
+
+    #[test]
+    fn test_nfstime4_negative_seconds() {
+        let time = NfsTime4 {
+            seconds: -1,
+            nseconds: 500_000_000,
+        };
+
+        let mut w = XdrWriter::new();
+        time.encode(&mut w).unwrap();
+
+        let mut r = XdrReader::new(w.as_bytes());
+        let decoded = NfsTime4::decode(&mut r).unwrap();
+
+        assert_eq!(time, decoded);
+    }
 }
