2e3c7fedd215196b7ce31e3b89d36dfbe0617d17
Hiroyuki Sato <hiroysato@gmail.com>
2026-03-19 09:28:15 +0900

    Complete NfsStat4 enum definitions

--- src/nfsv4/types/nfsstat4.rs
+++ src/nfsv4/types/nfsstat4.rs
@@ -5,10 +5,138 @@ use crate::nfsv4::types::Nfsv4Error;
 use xdr_rs::reader::XdrReader;
 use xdr_rs::writer::XdrWriter;
 
-/// RFC7531: nfsstat4 (partial)
+/// RFC7531: nfsstat4
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(i32)]
 pub enum NfsStat4 {
+    /// NFS4_OK - everything is okay
+    Ok = 0,
+
+    /// NFS4ERR_PERM - caller not privileged
+    Perm = 1,
+
+    /// NFS4ERR_NOENT - no such file/directory
+    NoEnt = 2,
+
+    /// NFS4ERR_IO - hard I/O error
+    Io = 5,
+
+    /// NFS4ERR_NXIO - no such device
+    NxIo = 6,
+
+    /// NFS4ERR_ACCESS - access denied
+    Access = 13,
+
+    /// NFS4ERR_EXIST - file already exists
+    Exist = 17,
+
+    /// NFS4ERR_XDEV - different file systems
+    XDev = 18,
+
+    /// Unused / Reserved   _Unused1 = 19,
+
+    /// NFS4ERR_NOTDIR - should be a directory
+    NotDir = 20,
+
+    /// NFS4ERR_ISDIR - should not be directory
+    IsDir = 21,
+
+    /// NFS4ERR_INVAL - invalid argument
+    Inval = 22,
+
+    /// NFS4ERR_FBIG - file exceeds server max
+    FBig = 27,
+
+    /// NFS4ERR_NOSPC - no space on file system
+    NoSpc = 28,
+
+    /// NFS4ERR_ROFS - read-only file system
+    Rofs = 30,
+
+    /// NFS4ERR_MLINK - too many hard links
+    Mlink = 31,
+
+    /// NFS4ERR_NAMETOOLONG - name exceeds server max
+    NameTooLong = 63,
+
+    /// NFS4ERR_NOTEMPTY - directory not empty
+    NotEmpty = 66,
+
+    /// NFS4ERR_DQUOT - hard quota limit reached
+    Dquot = 69,
+
+    /// NFS4ERR_STALE - file no longer exists
+    Stale = 70,
+
+    /// NFS4ERR_BADHANDLE - Illegal filehandle
+    BadHandle = 10001,
+
+    /// NFS4ERR_BAD_COOKIE - READDIR cookie is stale
+    BadCookie = 10003,
+
+    /// NFS4ERR_NOTSUPP - operation not supported
+    NotSupp = 10004,
+
+    /// NFS4ERR_TOOSMALL - response limit exceeded
+    TooSmall = 10005,
+
+    /// NFS4ERR_SERVERFAULT - undefined server error
+    ServerFault = 10006,
+
+    /// NFS4ERR_BADTYPE - type invalid for CREATE
+    BadType = 10007,
+
+    /// NFS4ERR_DELAY - file "busy" - retry
+    Delay = 10008,
+
+    /// NFS4ERR_SAME - nverify says attrs same
+    Same = 10009,
+
+    /// NFS4ERR_DENIED - lock unavailable
+    Denied = 10010,
+
+    /// NFS4ERR_EXPIRED - lock lease expired
+    Expired = 10011,
+
+    /// NFS4ERR_LOCKED - I/O failed due to lock
+    Locked = 10012,
+
+    /// NFS4ERR_GRACE - in grace period
+    Grace = 10013,
+
+    /// NFS4ERR_FHEXPIRED - filehandle expired
+    FhExpired = 10014,
+
+    /// NFS4ERR_SHARE_DENIED - share reserve denied
+    ShareDenied = 10015,
+
+    /// NFS4ERR_WRONGSEC - wrong security flavor
+    WrongSec = 10016,
+
+    /// NFS4ERR_CLID_INUSE - clientid in use
+    ClidInUse = 10017,
+
+    /// NFS4ERR_RESOURCE - resource exhaustion
+    Resource = 10018,
+
+    /// NFS4ERR_MOVED - file system relocated
+    Moved = 10019,
+
+    /// NFS4ERR_NOFILEHANDLE - current FH is not set
+    NoFileHandle = 10020,
+
+    /// NFS4ERR_MINOR_VERS_MISMATCH - minor vers not supp
+    MinorVersMismatch = 10021,
+
+    /// NFS4ERR_STALE_CLIENTID - server has rebooted
+    StaleClientId = 10022,
+
+    /// NFS4ERR_STALE_STATEID - server has rebooted
+    StaleStateId = 10023,
+
+    /// NFS4ERR_OLD_STATEID - state is out of sync
+    OldStateId = 10024,
+
     /// NFS4ERR_BAD_STATEID - incorrect stateid
     BadStateId = 10025,
 
