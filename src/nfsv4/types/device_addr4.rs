503068691d85cc9d6a5f6fc445b533cd5f00f8d2
Hiroyuki Sato <hiroysato@gmail.com>
2026-03-22 01:03:22 +0900

    Add DeviceAddr4 tests

--- src/nfsv4/types/device_addr4.rs
+++ src/nfsv4/types/device_addr4.rs
@@ -1,10 +1,9 @@
 #![allow(dead_code)]
 
-use super::*;
-
 use xdr_rs::reader::XdrReader;
 use xdr_rs::writer::XdrWriter;
 
+use crate::nfsv4::types::LayoutType4;
 use crate::nfsv4::types::Nfsv4Error;
 
 /// RFC8881 Section 3.3.15: device_addr4
@@ -33,5 +32,63 @@ impl DeviceAddr4 {
         w.write_i32(self.da_layout_type as i32)?;
         w.write_opaque(&self.da_addr_body)?;
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_deviceaddr4_encode_decode() {
+        let original = DeviceAddr4 {
+            da_layout_type: LayoutType4::NfsV4_1Files,
+            da_addr_body: vec![0x01, 0x02, 0x03, 0x04],
+        };
+
+        let mut w = XdrWriter::new();
+        original.encode(&mut w).unwrap();
+
+        let mut r = XdrReader::new(w.as_bytes());
+        let decoded = DeviceAddr4::decode(&mut r).unwrap();
+
+        assert_eq!(original, decoded);
+    }
+
+    #[test]
+    fn test_deviceaddr4_empty_body() {
+        let original = DeviceAddr4 {
+            da_layout_type: LayoutType4::BlockVolume,
+            da_addr_body: vec![],
+        };
+
+        let mut w = XdrWriter::new();
+        original.encode(&mut w).unwrap();
+
+        let mut r = XdrReader::new(w.as_bytes());
+        let decoded = DeviceAddr4::decode(&mut r).unwrap();
+
+        assert_eq!(original, decoded);
+    }
+
+    #[test]
+    fn test_deviceaddr4_decode_invalid_layout_type() {
+        let mut w = XdrWriter::new();
+        w.write_i32(99).unwrap();
+        w.write_opaque(&[0xaa, 0xbb]).unwrap();
+
+        let mut r = XdrReader::new(w.as_bytes());
+        let err = DeviceAddr4::decode(&mut r).unwrap_err();
+
+        assert!(matches!(err, Nfsv4Error::InvalidLayoutType(99)));
+    }
+
+    #[test]
+    fn test_deviceaddr4_decode_truncated_body() {
+        let buf = [0x00, 0x00, 0x00, 0x01, 0x00, 0x00];
+        let mut r = XdrReader::new(&buf);
+
+        let err = DeviceAddr4::decode(&mut r).unwrap_err();
+        assert!(matches!(err, Nfsv4Error::Xdr(_)));
     }
 }
