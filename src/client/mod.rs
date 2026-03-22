cda206f26aff6891adae323d164edaf8bee1d709
Hiroyuki Sato <hiroysato@gmail.com>
2026-03-22 22:42:14 +0900

    Remove client module from nfsv4-rs

--- src/client/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod config;
-pub mod low_level;
-pub mod options;
-pub mod session;
-
-pub use config::{AuthConfig, LowLevelClientConfig, NfsMinorVersion, ServerAddr};
-pub use low_level::LowLevelClient;
-pub use options::RpcCallOptions;
-pub use session::SessionState;
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-pub mod client;
 pub mod error;
 pub mod nfsv4;
 pub mod xdr_ext;
