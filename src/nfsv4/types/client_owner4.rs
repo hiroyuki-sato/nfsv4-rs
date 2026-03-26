589f2435bd9322a2c1caea302c47fa6922b6ae15
Hiroyuki Sato <hiroysato@gmail.com>
2026-03-26 12:02:27 +0900

    Add ClientOwner4 constructor and verifier generator with tests

--- src/nfsv4/types/client_owner4.rs
+++ src/nfsv4/types/client_owner4.rs
@@ -1,5 +1,8 @@
 #![allow(dead_code)]
 
+use std::fs::File;
+use std::io::Read;
+
 use xdr_rs::reader::XdrReader;
 use xdr_rs::writer::XdrWriter;
 
@@ -20,6 +23,13 @@ pub struct ClientOwner4 {
 }
 
 impl ClientOwner4 {
+    pub fn new(verifier: Verifier4, owner: &str) -> Self {
+        Self {
+            co_verifier: verifier,
+            co_ownerid: owner.as_bytes().to_vec(),
+        }
+    }
+
     pub fn decode(r: &mut XdrReader) -> Result<Self, Nfsv4Error> {
         let co_verifier: Verifier4 = r
             .read_fixed_opaque(VERIFIER_SIZE)?
@@ -36,6 +46,13 @@ impl ClientOwner4 {
         w.write_fixed_opaque(&self.co_verifier)?;
         w.write_opaque(&self.co_ownerid)?;
         Ok(())
+    }
+
+    pub fn generate_verifier() -> Verifier4 {
+        let mut v = [0u8; VERIFIER_SIZE];
+        let mut f = File::open("/dev/urandom").unwrap();
+        f.read_exact(&mut v).unwrap();
+        v
     }
 }
 
@@ -83,5 +100,38 @@ mod tests {
         let err = ClientOwner4::decode(&mut r).unwrap_err();
 
         assert!(matches!(err, Nfsv4Error::Xdr(_)));
+    }
+
+    #[test]
+    fn test_clientowner4_new() {
+        let verifier: Verifier4 = [1, 2, 3, 4, 5, 6, 7, 8];
+        let owner = "test-client";
+
+        let obj = ClientOwner4::new(verifier, owner);
+
+        assert_eq!(obj.co_verifier, verifier);
+        assert_eq!(obj.co_ownerid, owner.as_bytes().to_vec());
+    }
+
+    #[test]
+    fn test_generate_verifier_length() {
+        let v = ClientOwner4::generate_verifier();
+
+        assert_eq!(v.len(), VERIFIER_SIZE);
+    }
+
+    #[test]
+    fn test_generate_verifier_not_all_zero() {
+        let v = ClientOwner4::generate_verifier();
+
+        assert!(v.iter().any(|&b| b != 0));
+    }
+
+    #[test]
+    fn test_generate_verifier_uniqueness() {
+        let v1 = ClientOwner4::generate_verifier();
+        let v2 = ClientOwner4::generate_verifier();
+
+        assert_ne!(v1, v2);
     }
 }
